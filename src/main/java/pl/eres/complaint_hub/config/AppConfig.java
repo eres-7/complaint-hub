fbe6fc76df4836e714b5c10f64dda876f04108b0
Rafał <rafal.sobczak@pentacomp.pl>
2025-03-15 05:14:00 +0100

    Custom base url for swagger UI

--- src/main/java/pl/eres/complaint_hub/config/AppConfig.java
+++ src/main/java/pl/eres/complaint_hub/config/AppConfig.java
@@ -1,5 +1,8 @@
 package pl.eres.complaint_hub.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -10,5 +13,10 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public OpenAPI customSwaggerBaseUrl(@Value("${springdoc.server.base-url:http://localhost:8080}") String springdocServerBaseUrl) {
+        return new OpenAPI().addServersItem(new Server().url(springdocServerBaseUrl));
     }
 }
